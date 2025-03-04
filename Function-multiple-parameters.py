c219dca21705e641d5f30dc35f391f97857007e5
kresimir.papic@hotmail.com <kresimir.papic@hotmail.com>
2025-03-04 14:21:54 +0100

    Adding Function-multiple-parameters.py

---
+++ Function-multiple-parameters.py
@@ -0,0 +1,9 @@
+def calculate_rectangle_area(width, height):
+     return width * height
+
+width = int(input("Please enter width of a rectangle: "))
+height = int(input("Please enter height of reactangle: "))
+
+area = calculate_rectangle_area(width, height)
+print(f"Area is {area}")
+
